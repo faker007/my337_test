b37502c97b5cf681e7e3e98c8e2145257d096e7e
LeeGyeJin <rlaqjadud195@naver.com>
2016-09-26 16:28:57 +0900

    editing promise.js

--- promise.js
+++ promise.js
@@ -1,2 +1,7 @@
-var os = require('os');
-console.log(os.platform());
+function setAlarm(time) {
+	setTimeout(function () {
+		console.log(time / 1000 + "초가 지났습니다.");
+	}, time)
+}
+
+setAlarm(3500);
